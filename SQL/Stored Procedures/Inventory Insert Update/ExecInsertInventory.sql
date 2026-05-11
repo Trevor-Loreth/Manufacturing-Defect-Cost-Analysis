30bb0e78a0f99260de7c0fff406a616082c8c083
Trevor-Loreth <tloreth@gmail.com>
2026-05-11 11:06:41 -0700

    Create ExecInsertInventory.sql

---
+++ SQL/Stored Procedures/Inventory Insert Update/ExecInsertInventory.sql
@@ -0,0 +1,6 @@
+EXEC InsertInventory
+    @ProductSerialNumber = 'P-103271',
+    @BladesSerialNumber = 'B-503271',
+    @MotorSerialNumber = 'M-703271',
+    @FrameSerialNumber = 'F-903271',
+    @AssemblyCost = 21.00;
