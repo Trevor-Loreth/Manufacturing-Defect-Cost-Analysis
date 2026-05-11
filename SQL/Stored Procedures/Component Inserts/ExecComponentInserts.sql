584b6438b4cc6231b896f3d634f1c126a808605c
Trevor-Loreth <tloreth@gmail.com>
2026-05-11 11:16:34 -0700

    Create ExecComponentInserts.sql

---
+++ SQL/Stored Procedures/Component Inserts/ExecComponentInserts.sql
@@ -0,0 +1,11 @@
+EXEC InsertBlade
+    @BladesSerialNumber = 'B-503271',
+    @Factory = 'Seattle WA';
+
+EXEC InsertFrame
+    @FrameSerialNumber = 'F-903271',
+    @Factory = 'Dallas TX';
+
+EXEC InsertMotor
+    @MotorSerialNumber = 'M-703271',
+    @Factory = 'Denver CO';
